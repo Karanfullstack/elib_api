90223becf373d41ef1c63df7b2a09dd2e3498d8c
Karan Chauhan <karanchouhan211@gmail.com>
2024-07-01 15:51:00 +0200

    book create complete

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
 import { uploadService } from '../utils/upload.service'
+import { BookI } from './book.types'
+import BookModel from './book.model'
+import createHttpError from 'http-errors'
 
 export const createBook = async (
     req: Request,
@@ -7,9 +10,29 @@ export const createBook = async (
     next: NextFunction
 ) => {
     const files = req.files as { [fieldname: string]: Express.Multer.File[] }
-    const cover = await uploadService(files, 'coverImage')
-    const pdffile = await uploadService(files, 'file')
-    console.log(pdffile, cover)
-    // console.log(result)
-    return res.status(200).json({ message: 'Book created' })
+    const { title, description, genere, author } = req.body as BookI
+
+    let cover
+    let pdffile
+
+    try {
+        cover = await uploadService(files, 'coverImage')
+        pdffile = await uploadService(files, 'file')
+    } catch (error) {
+        return next(createHttpError(400, 'Error uploading files'))
+    }
+
+    try {
+        const book = await BookModel.create({
+            title,
+            description,
+            coverImage: cover.secure_url,
+            file: pdffile.secure_url,
+            author,
+            genere,
+        })
+        return res.status(200).json({ message: 'Book created', book: book })
+    } catch (error) {
+        return next(createHttpError(400, 'Error creating book'))
+    }
 }
