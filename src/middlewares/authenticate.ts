6b4b68ebbe652ef2b0469617895cd08f537bcf2d
Karan Chauhan <karanchouhan211@gmail.com>
2024-07-01 19:05:01 +0200

    auth middleware working

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -10,19 +10,19 @@ export const authenticate = (
     res: Response,
     next: NextFunction
 ) => {
-    return next()
-    // try {
-    //     const token = req.header('Authorization')
-    //     if (!token) {
-    //         return next(createHttpError(401, 'Authorized token is required'))
-    //     }
+    try {
+        const token = req.header('Authorization')
+        if (!token) {
+            return next(createHttpError(401, 'Authorized token is required'))
+        }
 
-    //     const parseToken = token.split(' ')[1]
-    //     const decoded = verifyToken(parseToken)
-    //     const _req = req as AuthRequest
-    //     _req.userId = decoded.sub as string
-    //     next()
-    // } catch (error) {
-    //     return next(createHttpError(401, 'Token expired'))
-    // }
+        const parseToken = token.split(' ')[1]
+        const decoded = verifyToken(parseToken)
+        const _req = req as AuthRequest
+        _req.userId = decoded.sub as string
+        console.log(decoded)
+        next()
+    } catch (error) {
+        return next(createHttpError(401, 'Token expired'))
+    }
 }
